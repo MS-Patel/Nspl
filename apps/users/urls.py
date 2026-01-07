9836e533e459c4190a9e092df23d78eea4f81b79
MS-Patel <patelmilind1620@gmail.com>
2026-01-07 10:19:31 +0530

    changes

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('login/', views.CustomLoginView.as_view(), name='login'),
-    path('logout/', views.CustomLogoutView.as_view(), name='logout'),
+    path('accounts/login/', views.CustomLoginView.as_view(), name='login'),
+    path('accounts/logout/', views.CustomLogoutView.as_view(), name='logout'),
 
     # Dashboards
     path('dashboard/admin/', views.AdminDashboardView.as_view(), name='admin_dashboard'),
