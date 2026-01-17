897e7450c94e06dc571b998dc09eba44a93c4c2e
MS-Patel <patelmilind1620@gmail.com>
2026-01-17 22:14:53 +0530

    changes

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import User
+from .models import InvestorProfile, User
 # Register your models here.
 
 admin.site.register(User)  # Add your user model here inside the register() method
+admin.site.register(InvestorProfile)  # Add your user model here inside the register() method
