0fc19f53419be35fb1595da385f0505fbb41bcd0
MS-Patel <patelmilind1620@gmail.com>
2026-01-19 09:55:44 +0530

    changes

--- apps/investments/admin.py
+++ apps/investments/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from apps.investments.models import Mandate, Order
+
 # Register your models here.
+
+
+admin.site.register(Mandate)  # Add your investment models here
+admin.site.register(Order)  # Add your investment models here
